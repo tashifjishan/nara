8794900a4a900a5aa594d4793af3c7169fedad69
pradhan <kpradhan2933@gmail.com>
2025-02-18 15:14:18 +0530

    test

--- controllers/auth.js
+++ controllers/auth.js
@@ -28,7 +28,7 @@ const createSendToken = async (user, req, res) => {
     res.cookie("jwt", token, {
         expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
         httpOnly: true,        
-        secure: true, 
+        secure: false, 
         sameSite: 'None'
     });
     
