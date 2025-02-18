b8e9827a72975f6fba6c01db07e4ee9389073beb
pradhan <kpradhan2933@gmail.com>
2025-02-18 15:19:38 +0530

    test

--- controllers/auth.js
+++ controllers/auth.js
@@ -29,7 +29,7 @@ const createSendToken = async (user, req, res) => {
         expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
         httpOnly: true,        
         secure: false, 
-       
+        sameSite: 'None'
     });
     
     user.password = undefined;
